d6f83ace535d8da85e11b63e078a137fbdbb13a6
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2021-03-12 09:42:43 -0800

    Selecting Elements and Changing CSS style

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-$("h1").css("color", "red") //changes h1 color to red
+$("button"); //Selects all button elements
+$("h1").addClass("big-title") //Selects element.manipulates style -> Adds CSS style to element
+$("h1").removeClass("big-title") //Removes CSS style from element
+$("h1").addClass("big-title margin-50") //Add multiple CSS style classes to element
+$("h1").hasClass("margin-50") //returns true or false boolean
+
